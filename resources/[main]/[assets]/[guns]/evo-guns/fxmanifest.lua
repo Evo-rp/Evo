3f35d09b47793e8745fe6ce7ff5de42cca5f9d79
BlazerBoccle <152270436+BlazerBoccle@users.noreply.github.com>
2024-05-01 11:28:39 +0100

    p

---
+++ resources/[main]/[assets]/[guns]/evo-guns/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+client_script 'main.lua'
+
+files{
+	'**/*.dat',
+	'**/*.meta',
+}
+
+data_file 'WEAPONCOMPONENTSINFO_FILE' '**/weaponcomponents.meta'
+data_file 'WEAPON_METADATA_FILE' '**/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' '**/weaponanimations.meta'
+data_file 'PED_PERSONALITY_FILE' '**/pedpersonality.meta'
+data_file 'WEAPONINFO_FILE' '**/weapons.meta'
+data_file "LOADOUTS_FILE" "**/loadouts.meta"
+data_file "EXPLOSION_INFO_FILE" "**/explosion.meta"
+data_file "EXPLOSIONFX_FILE" "**/explosionfx.dat"
