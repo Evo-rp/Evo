affff20f5a89921ec29788acbd422ac82ee33307
BlazerBoccle <152270436+BlazerBoccle@users.noreply.github.com>
2024-05-01 11:24:21 +0100

    start bat

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
 cd /d D:\fivem\Evo
-D:\fivem\[artifacts]\FXServer.exe +exec configs/server.cfg +set gamename gta +set sv_enforceGameBuild 2944 +set onesync on
+D:\fivem\[artifacts]\FXServer.exe +exec config/server.cfg +set gamename gta +set sv_enforceGameBuild 2944 +set onesync on
