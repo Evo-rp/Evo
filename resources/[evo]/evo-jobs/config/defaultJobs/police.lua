426dea245c5ad0d74e3f7d0389e2eb2c4f171252
Lexie <54323871+AiLexie@users.noreply.github.com>
2024-05-05 19:24:46 -0400

    police update

--- resources/[evo]/evo-jobs/config/defaultJobs/police.lua
+++ resources/[evo]/evo-jobs/config/defaultJobs/police.lua
@@ -3,9 +3,119 @@ table.insert(_defaultJobData, {
 	LastUpdated = 1682170258,
 	Id = "police",
 	Name = "Police",
-	Salary = 1200,
+	Salary = 1100,
 	SalaryTier = 1,
 	Workplaces = {
+		{
+            Id = 'sapr',
+            Name = 'San Andreas Park Rangers',
+            Grades = {
+                {
+                    Id = 'warden',
+                    Name = 'Game Warden',
+                    Level = 99,
+                    Salary = 5000,
+                    Permissions = {
+                        PD_HIGH_COMMAND = true,
+                    },
+                },
+                {
+                    Id = 'dwarden',
+                    Name = 'Assistant Game Warden',
+                    Level = 90,
+                    Salary = 4500,
+                    Permissions = {
+                        PD_HIGH_COMMAND = true,
+                    },
+                },
+                {
+                    Id = 'awarden',
+                    Name = 'Deputy Game Warden',
+                    Level = 80,
+                    Salary = 4000,
+                    Permissions = {
+                        PD_HIGH_COMMAND = true,
+                    },
+                },
+                {
+                    Id = 'major',
+                    Name = 'Major',
+                    Level = 75,
+                    Salary = 3750,
+                    Permissions = {
+                        PD_HIGH_COMMAND = true,
+                        },
+                },
+                {
+                    Id = 'captain',
+                    Name = 'Captain',
+                    Level = 70,
+                    Salary = 3500,
+                    Permissions = {
+                        PD_COMMAND = true,
+                        },
+                },
+                {
+                    Id = 'lieutenant',
+                    Name = 'Lieutenant',
+                    Level = 60,
+                    Salary = 3000,
+                    Permissions = {
+                        PD_COMMAND = true,
+                    },
+                },
+                {
+                    Id = 'sergeant',
+                    Name = 'Sergeant',
+                    Level = 50,
+                    Salary = 2500,
+                    Permissions = {
+                        PD_SUPERVISOR = true,
+                    },
+                },
+                {
+                    Id = 'mranger',
+                    Name = 'Ranger 1st Class',
+                    Level = 20,
+                    Salary = 2000,
+                    Permissions = {
+                        PD_SUPERVISOR = true,
+                    },
+                },
+                {
+                    Id = 'sranger',
+                    Name = 'Senior Ranger',
+                    Level = 15,
+                    Salary = 1250,
+                    Permissions = {
+                    },
+                },
+                {
+                    Id = 'ranger',
+                    Name = 'Ranger',
+                    Level = 10,
+                    Salary = 950,
+                    Permissions = {
+                    },
+                },
+                {
+                    Id = 'pranger',
+                    Name = 'Probationary Ranger',
+                    Level = 2,
+                    Salary = 500,
+                    Permissions = {
+                    },
+                },
+                {
+                    Id = 'cadet',
+                    Name = 'Cadet',
+                    Level = 1,
+                    Salary = 250,
+                    Permissions = {
+                    },
+                },
+            }
+        },
 		{
 			Id = "lspd",
 			Name = "Los Santos Police Department",
@@ -262,8 +372,8 @@ table.insert(_defaultJobData, {
 			},
 		},
 		{
-			Id = "bcso",
-			Name = "Blaine County Sheriff's Office",
+			Id = "lscso",
+			Name = "Los Santos Sheriff\'s Office",
 			Grades = {
 				{
 					Id = "sheriff",
@@ -767,9 +877,9 @@ table.insert(_defaultJobData, {
 						MDT_CIVILIAN_REPORT_CREATE = true,
 						JOB_STORAGE = true,
 						impound = true,
-					},
-				},
-			},
-		},
-	},
+                    },
+                },
+            }
+        },
+    }
 })
