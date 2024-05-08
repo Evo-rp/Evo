4f11aa34ad77f010f462596daf1ac6b687e2b690
Blooper <118882930+Blooper4921@users.noreply.github.com>
2024-05-08 23:50:10 +0100

    Update goldpan.lua

--- resources/[evo]/evo-labor/client/jobs/goldpan.lua
+++ resources/[evo]/evo-labor/client/jobs/goldpan.lua
@@ -2,15 +2,6 @@ RegisterNetEvent("Characters:Client:Spawn", function()
     Blips:Add("GoldPan", "Goldpanning Utilities", { x = 1956.636, y = 710.202, z = 163.387 }, 478, 46, 0.9)
 end)
 
-Callbacks:RegisterClientCallback("Labor:Goldpanning:StartBucket", function(data, cb)
-    local waterLevel = GetEntitySubmergedLevel(PlayerPedId())
-    if waterLevel > 0 and waterLevel < 0.8 then
-        SkillCheckBucket()
-    else
-        Notification:Error("You aren't in shallow enough water fill your bucket", 2000)
-    end
-end)
-
 function SkillCheckBucket()
     local p = promise.new()
     local model = `prop_bucket_02a` 
@@ -70,19 +61,6 @@ function SkillCheckBucket()
     return Citizen.Await(p)
 end
 
-Callbacks:RegisterClientCallback("Labor:Goldpanning:StartSifting", function(data, cb)
-    if Inventory.Check.Player:HasItem('gravel', 5) then
-        local waterLevel = GetEntitySubmergedLevel(PlayerPedId())
-        if waterLevel > 0 and waterLevel < 0.8 then
-            SkillCheckPanning()
-        else
-            Notification:Error("You aren't in shallow enough water to sift for gold", 2000)
-        end
-    else
-        Notification:Error("You don't have a enough gravel", 2000)
-    end
-end)
-
 function SkillCheckPanning()
     local p = promise.new()
     Minigame.Play:RoundSkillbar(2.0, 2, {
@@ -133,19 +111,43 @@ local SmeltingCoords = {
     heading = 325,
 }
 
-Callbacks:RegisterClientCallback("Labor:Goldpanning:StartMoulding", function(data, cb)
-    if Inventory.Check.Player:HasItem('goldnugget', 20) then
-        local playerPos = GetEntityCoords(PlayerPedId())
-        local distance = #(playerPos - SmeltingCoords.position)
-        if distance <= SmeltingCoords.size.x / 2 then
-            SmeltBars()
+function RegisterCallbacks()
+    Callbacks:RegisterClientCallback("Labor:Goldpanning:StartMoulding", function(data, cb)
+        if Inventory.Check.Player:HasItem('goldnugget', 20) then
+            local playerPos = GetEntityCoords(PlayerPedId())
+            local distance = #(playerPos - SmeltingCoords.position)
+            if distance <= SmeltingCoords.size.x / 2 then
+                SmeltBars()
+            else
+                Notification:Error("You aren't close enough to the smelting area", 2000)
+            end
         else
-            Notification:Error("You aren't close enough to the smelting area", 2000)
+            Notification:Error("You don't have enough gold nuggets", 2000)
         end
-    else
-        Notification:Error("You don't have enough gold nuggets", 2000)
-    end
-end)
+    end)
+
+    Callbacks:RegisterClientCallback("Labor:Goldpanning:StartBucket", function(data, cb)
+        local waterLevel = GetEntitySubmergedLevel(PlayerPedId())
+        if waterLevel > 0 and waterLevel < 0.8 then
+            SkillCheckBucket()
+        else
+            Notification:Error("You aren't in shallow enough water fill your bucket", 2000)
+        end
+    end)
+
+    Callbacks:RegisterClientCallback("Labor:Goldpanning:StartSifting", function(data, cb)
+        if Inventory.Check.Player:HasItem('gravel', 5) then
+            local waterLevel = GetEntitySubmergedLevel(PlayerPedId())
+            if waterLevel > 0 and waterLevel < 0.8 then
+                SkillCheckPanning()
+            else
+                Notification:Error("You aren't in shallow enough water to sift for gold", 2000)
+            end
+        else
+            Notification:Error("You don't have a enough gravel", 2000)
+        end
+    end)
+end
 
 function SmeltBars()
     local p = promise.new()
