e3d8029d93a2cb91e5aaaac11bd49bd0ee984e1e
BlazerBoccle <152270436+BlazerBoccle@users.noreply.github.com>
2024-05-22 12:28:54 +0100

    upd(sql) Database sql

--- [SQL]/database.sql
+++ [SQL]/database.sql
@@ -1,23 +1,98 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               10.6.12-MariaDB - mariadb.org binary distribution
+-- Server version:               11.1.2-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
--- HeidiSQL Version:             11.3.0.6295
+-- HeidiSQL Version:             12.3.0.6589
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Dumping database structure for fivem-evo_prod
-CREATE DATABASE IF NOT EXISTS `fivem-evo_prod` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
-USE `fivem-evo_prod`;
+-- Dumping database structure for evo
+CREATE DATABASE IF NOT EXISTS `evo` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
+USE `evo`;
 
--- Dumping structure for table fivem-evo_prod.crafting_cooldowns
+-- Dumping structure for table evo.bank_accounts
+CREATE TABLE IF NOT EXISTS `bank_accounts` (
+  `account` int(10) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `owner` varchar(255) NOT NULL,
+  `balance` int(10) NOT NULL DEFAULT 0,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`account`) USING BTREE,
+  KEY `Owner` (`owner`) USING BTREE,
+  KEY `Type` (`type`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table evo.bank_accounts_permissions
+CREATE TABLE IF NOT EXISTS `bank_accounts_permissions` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `account` int(10) NOT NULL,
+  `type` int(10) NOT NULL,
+  `jointOwner` varchar(255) DEFAULT NULL,
+  `job` varchar(255) DEFAULT NULL,
+  `workplace` varchar(255) DEFAULT NULL,
+  `jobPermissions` varchar(1024) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `job` (`job`) USING BTREE,
+  KEY `workplace` (`workplace`) USING BTREE,
+  KEY `jointOwner` (`jointOwner`) USING BTREE,
+  KEY `account` (`account`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=87 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table evo.bank_accounts_transactions
+CREATE TABLE IF NOT EXISTS `bank_accounts_transactions` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) NOT NULL,
+  `account` int(10) NOT NULL,
+  `amount` int(10) NOT NULL DEFAULT 0,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `description` varchar(1024) NOT NULL DEFAULT '',
+  `data` varchar(1024) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `account` (`account`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table evo.bans
+CREATE TABLE IF NOT EXISTS `bans` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tokens` varchar(255) NOT NULL,
+  `expires` bigint(20) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT 1,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table evo.ban_tokens
+CREATE TABLE IF NOT EXISTS `ban_tokens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ban_id` int(11) DEFAULT NULL,
+  `token` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ban_id` (`ban_id`),
+  CONSTRAINT `ban_tokens_ibfk_1` FOREIGN KEY (`ban_id`) REFERENCES `bans` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table evo.crafting_cooldowns
 CREATE TABLE IF NOT EXISTS `crafting_cooldowns` (
   `bench` varchar(64) NOT NULL,
   `id` varchar(64) NOT NULL,
@@ -26,7 +101,7 @@ CREATE TABLE IF NOT EXISTS `crafting_cooldowns` (
 
 -- Data exporting was unselected.
 
--- Dumping structure for table fivem-evo_prod.inventory
+-- Dumping structure for table evo.inventory
 CREATE TABLE IF NOT EXISTS `inventory` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL DEFAULT '0',
@@ -39,11 +114,11 @@ CREATE TABLE IF NOT EXISTS `inventory` (
   `expiryDate` bigint(20) NOT NULL DEFAULT -1,
   PRIMARY KEY (`id`) USING BTREE,
   KEY `name` (`name`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=1164831 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1164850 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table fivem-evo_prod.inventory_shop_logs
+-- Dumping structure for table evo.inventory_shop_logs
 CREATE TABLE IF NOT EXISTS `inventory_shop_logs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `date` datetime NOT NULL DEFAULT current_timestamp(),
@@ -58,7 +133,7 @@ CREATE TABLE IF NOT EXISTS `inventory_shop_logs` (
 
 -- Data exporting was unselected.
 
--- Dumping structure for table fivem-evo_prod.meth_tables
+-- Dumping structure for table evo.meth_tables
 CREATE TABLE IF NOT EXISTS `meth_tables` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `tier` int(11) NOT NULL DEFAULT 1,
@@ -71,7 +146,7 @@ CREATE TABLE IF NOT EXISTS `meth_tables` (
 
 -- Data exporting was unselected.
 
--- Dumping structure for table fivem-evo_prod.placed_meth_tables
+-- Dumping structure for table evo.placed_meth_tables
 CREATE TABLE IF NOT EXISTS `placed_meth_tables` (
   `table_id` int(11) NOT NULL,
   `owner` bigint(20) DEFAULT NULL,
@@ -84,7 +159,7 @@ CREATE TABLE IF NOT EXISTS `placed_meth_tables` (
 
 -- Data exporting was unselected.
 
--- Dumping structure for table fivem-evo_prod.placed_props
+-- Dumping structure for table evo.placed_props
 CREATE TABLE IF NOT EXISTS `placed_props` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `model` varchar(255) NOT NULL DEFAULT '',
@@ -101,70 +176,36 @@ CREATE TABLE IF NOT EXISTS `placed_props` (
 
 -- Data exporting was unselected.
 
-CREATE TABLE `bank_accounts` (
-	`account` INT(10) NOT NULL,
-	`type` VARCHAR(255) NOT NULL COLLATE 'latin1_swedish_ci',
-	`owner` VARCHAR(255) NOT NULL COLLATE 'latin1_swedish_ci',
-	`balance` INT(10) NOT NULL DEFAULT '0',
-	`name` VARCHAR(255) NOT NULL COLLATE 'latin1_swedish_ci',
-	PRIMARY KEY (`account`) USING BTREE,
-	INDEX `Owner` (`owner`) USING BTREE,
-	INDEX `Type` (`type`) USING BTREE
-)
-COLLATE='latin1_swedish_ci'
-ENGINE=InnoDB;
+-- Dumping structure for table evo.sequence
+CREATE TABLE IF NOT EXISTS `sequence` (
+  `id` varchar(255) NOT NULL,
+  `sequence` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
-CREATE TABLE `bank_accounts_permissions` (
-	`id` INT(10) NOT NULL AUTO_INCREMENT,
-	`account` INT(10) NOT NULL,
-	`type` INT(10) NOT NULL,
-	`jointOwner` VARCHAR(255) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
-	`job` VARCHAR(255) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
-	`workplace` VARCHAR(255) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
-	`jobPermissions` VARCHAR(1024) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
-	PRIMARY KEY (`id`) USING BTREE,
-	INDEX `job` (`job`) USING BTREE,
-	INDEX `workplace` (`workplace`) USING BTREE,
-	INDEX `jointOwner` (`jointOwner`) USING BTREE,
-	INDEX `account` (`account`) USING BTREE
-)
-COLLATE='latin1_swedish_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=1;
+-- Data exporting was unselected.
 
+-- Dumping structure for table evo.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `forum` varchar(255) DEFAULT NULL,
+  `account` int(11) DEFAULT NULL,
+  `identifier` varchar(255) NOT NULL,
+  `verified` tinyint(1) DEFAULT NULL,
+  `joined` bigint(20) DEFAULT NULL,
+  `groups` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`groups`)),
+  `avatar` varchar(255) DEFAULT NULL,
+  `priority` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `identifier` (`identifier`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
-CREATE TABLE `bank_accounts_transactions` (
-	`id` INT(10) NOT NULL AUTO_INCREMENT,
-	`type` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`account` INT(10) NOT NULL,
-	`amount` INT(10) NOT NULL DEFAULT '0',
-	`timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`title` VARCHAR(255) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
-	`description` VARCHAR(1024) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
-	`data` VARCHAR(1024) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	PRIMARY KEY (`id`) USING BTREE,
-	INDEX `account` (`account`) USING BTREE
-)
-COLLATE='utf8mb4_general_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=1;
+-- Data exporting was unselected.
 
-CREATE TABLE bans (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    expires BIGINT NOT NULL,
-    active BOOLEAN NOT NULL DEFAULT TRUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-CREATE TABLE ban_tokens (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    ban_id INT,
-    token VARCHAR(255) NOT NULL,
-    FOREIGN KEY (ban_id) REFERENCES bans(id)
-);
-
-
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
