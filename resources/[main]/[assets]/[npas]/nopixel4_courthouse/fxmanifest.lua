3d4d94cac5abd8ea57ad3d7d2b15dd6ad6ce3d61
BlazerBoccle <152270436+BlazerBoccle@users.noreply.github.com>
2024-05-23 23:06:01 +0100

    Vanilla version fuck sake

--- resources/[main]/[assets]/[npas]/nopixel4_courthouse/fxmanifest.lua
+++ resources/[main]/[assets]/[npas]/nopixel4_courthouse/fxmanifest.lua
@@ -7,10 +7,14 @@ version '1.0.0'
 
 this_is_a_map 'yes'
 
+
+
 files {
     'np_4_courthouse_cache_y.dat',
-	'courthouse_doorsounds_game.dat151.rel'
+	'courthouse_doorsounds_game.dat151.rel',
+	'np4_court_timecycles.xml'
 }
 
 data_file 'AUDIO_GAMEDATA' 'courthouse_doorsounds_game.dat'
+data_file 'TIMECYCLEMOD_FILE' 'np4_court_timecycles.xml'
 dependency '/assetpacks'
--- resources/[main]/[assets]/[npas]/nopixel4_memorial_park/fxmanifest.lua
+++ resources/[main]/[assets]/[npas]/nopixel4_memorial_park/fxmanifest.lua
@@ -2,4 +2,9 @@ fx_version 'adamant'
 games { 'gta5' }
 
 this_is_a_map 'yes'
+lua54 'yes'
+
+escrow_ignore {
+    'replace_this_if_using_bobs74_ipl/nightclubs.lua',
+}
 dependency '/assetpacks'
