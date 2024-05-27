9f76a08c73602c5c6e904dcf3f729fda31e66d35
Zylozz <165849796+Zylozz@users.noreply.github.com>
2024-05-27 12:12:56 -0400

    Not finished but its started :)

--- resources/[evo]/evo-labor/server/startup.lua
+++ resources/[evo]/evo-labor/server/startup.lua
@@ -128,6 +128,13 @@ AddEventHandler("Labor:Server:Startup", function()
 		{ label = "Rank 4", value = 10000 },
 		{ label = "Rank 5", value = 12000 },
 	})
+	-- Labor.Jobs:Register("Scubba", "Scubba", 0, 2000, 100, false, {
+	-- 	{ label = "Rank 1", value = 1500 },
+	-- 	{ label = "Rank 2", value = 3000 },
+	-- 	{ label = "Rank 3", value = 7000 },
+	-- 	{ label = "Rank 4", value = 10000 },
+	-- 	{ label = "Rank 5", value = 12000 },
+	-- })
 	Labor.Jobs:Register("Farming", "Farming", 0, 1200, 100)
 	Labor.Jobs:Register("Salvaging", "Salvaging", 0, 1200, 100)
 	Labor.Jobs:Register("Garbage", "Garbage", 0, 1500, 100, false, {
