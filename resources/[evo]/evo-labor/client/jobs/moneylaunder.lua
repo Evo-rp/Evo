44d5ac1bf1a0ea0dcabc4fbeb9a39abb676bd9a6
Blooper <118882930+Blooper4921@users.noreply.github.com>
2024-05-28 00:46:44 +0100

    feat: evo-labor | Added money launder

--- resources/[evo]/evo-labor/client/jobs/moneylaunder.lua
+++ resources/[evo]/evo-labor/client/jobs/moneylaunder.lua
@@ -1,29 +1,61 @@
+local timesDone = 0
+local _InMoneyRun = false
+
 AddEventHandler("Labor:Client:Setup", function()
-    PedInteraction:Add("MoneylaunderPed", `a_m_m_farmer_01`, vector4(561.593, -1747.369, 33.443, 156.413), 63.569, 25.0, {
+    PedInteraction:Add("MoneylaunderPed", `s_m_y_dealer_01`, vector3(561.704, -1747.374, 32.443), 152.095, 25.0, {
 		{
 			icon = "hand-holding",
 			text = "Interact",
 			event = "Labor:Client:MoneyLaunder:SpeakToPed",
 		},
+		{
+			icon = "hand-holding",
+			text = "Trade Rolls",
+			event = "Labor:Client:MoneyLaunder:Trade",
+			data = {
+				item = 'moneyroll',
+				amount = 50
+			}
+		},
+		{
+			icon = "hand-holding",
+			text = "Trade Bands",
+			event = "Labor:Client:MoneyLaunder:Trade",
+			data = {
+				item = 'moneyband',
+				amount = 5
+			}
+		},
 	}, 'money-check-dollar-pen', 'WORLD_HUMAN_CLIPBOARD')
+
+	Interaction:RegisterMenu("moneyrun-cancel", "Cancel Money Run", "ban", function(data)
+		_InMoneyRun = false
+		timesDone = 0
+
+		PedInteraction:Remove("MoneyLaunderPed:Dropoff")
+
+		if _Blip ~= nil then
+			RemoveBlip(_Blip)
+		end
+	end, function()
+        return _InMoneyRun
+	end)
 end)
 
 AddEventHandler('Labor:Client:MoneyLaunder:SpeakToPed', function(data)
     NPCDialog.Open(data.entity, {
-		first_name = 'npcData[param.name].first_name',
-		last_name = 'npcData[param.name].last_name',
+		first_name = 'William',
+		last_name = 'Wash',
 		Tag = '💸',
-		description = 'npcData[param.name].description',
+		description = 'Are you sure ?',
 		buttons = {
 			{
-				label = 'Open Shop',
-				data = {
-					close = true,
-					event = 'Shop:Client:OpenShopNPC',
-					params = {
-						id = param.id
-					}
-				}
+				label = 'Start run!',
+				data = { close = true, event = 'Labor:Client:MoneyLaunder:GetLocation' }
+			},
+			{
+				label = 'Get checkques.',
+				data = { close = true }
 			},
 			{
 				label = 'See you later !',
@@ -31,4 +63,65 @@ AddEventHandler('Labor:Client:MoneyLaunder:SpeakToPed', function(data)
 			}
 		}
 	})
+end)
+
+AddEventHandler('Labor:Client:MoneyLaunder:GetLocation', function()
+	_InMoneyRun = true
+	Callbacks:ServerCallback('Labor:Server:MoneyLaunder:RetrieveLocations', {}, function(cb)
+		Location = math.random(#cb)
+
+		_Blip = Blips:Add("SalvDelivery", "Dropoff Location", vector3(cb[Location].x, cb[Location].y, cb[Location].z), 586, 2, 1.4)
+
+		SetEntityCoords(PlayerPedId(), cb[Location].x, cb[Location].y, cb[Location].z) -- Dev testing
+
+		PedInteraction:Add("MoneyLaunderPed:Dropoff", `ig_bankman`, vector3(cb[Location].x, cb[Location].y, cb[Location].z), cb[Location].w, 25.0, {
+			{
+				icon = 'money-bill',
+				text = "Interact",
+				event = "Labor:Client:Trade",
+			}
+		}, 'building-columns')
+	end)
+end)
+
+AddEventHandler('Labor:Client:Trade', function()
+	timesDone = timesDone + 1
+
+	if Inventory.Check.Player:HasItem('money_checkque', 1) then
+		PedInteraction:Remove("MoneyLaunderPed:Dropoff")
+
+		if _Blip ~= nil then
+			RemoveBlip(_Blip)
+		end
+
+		local Chance = math.random(1, 10)
+
+		if Chance < 3 then
+			Notification:Error('Checkque declined!', 1000)
+			Callbacks:ServerCallback('Labor:Server:MoneyLaunder:AlertPolice', { coords = GetEntityCoords(PlayerPedId() )}, function(callback) end)
+		end
+
+		if Chance > 3 then
+			Callbacks:ServerCallback('Labor:Server:MoneyLaunder:Complete')
+		end
+
+		if timesDone ~= 5 then
+			TriggerEvent('Labor:Client:MoneyLaunder:GetLocation')
+		else
+			timesDone = 0
+			_InMoneyRun = false
+
+			Callbacks:ServerCallback('Labor:Server:MoneyLaunder:Finish')
+		end
+	else
+		Notification:Info('You do not have a checkque to turn in!', 1000)
+	end
+end)
+
+AddEventHandler('Labor:Client:MoneyLaunder:Trade', function(_, data)
+	if Inventory.Check.Player:HasItem(data.item, data.amount) then
+		Callbacks:ServerCallback('Labor:Server:MoneyLaunder:Trade', { item = data.item }, function() end)
+	else
+		Notification:Error('You do not have the required items.', 1000)
+	end
 end)
---
+++ resources/[evo]/evo-labor/server/jobs/moneylaunder.lua
@@ -0,0 +1,82 @@
+local _Locations = {
+    vector4(145.193, -1037.378, 28.368, 253.865), -- Legion Square Bank
+    vector4(-1218.038, -331.987, 36.781, 290.761), -- Blvd Del Perro Bank
+    vector4(1180.520, 2705.429, 37.088, 84.504), -- Harmony Bank
+    vector4(-2964.328, 477.584, 14.697, 352.403), -- Great Ocean Bank
+    vector4(-99.867, 6470.790, 30.639, 125.275), -- Paleto Bank
+    vector4(309.742, -275.827, 53.165, 243.771), -- Hawick Bank 1
+    vector4(-355.210, -46.944, 48.036, 245.125), -- Hawick Bank 2
+}
+
+AddEventHandler("Labor:Server:Startup", function()
+    Callbacks:RegisterServerCallback('Labor:Server:MoneyLaunder:RetrieveLocations', function(source, data, cb)
+        Phone.Notification:Add(
+			source,
+			"Labor Activity",
+			"Head to the location on your GPS",
+			os.time() * 1000,
+			6000,
+			"labor",
+			{}
+		)
+
+        cb(_Locations)
+    end)
+
+    Callbacks:RegisterServerCallback('Labor:Server:MoneyLaunder:Complete', function(source, data, cb)
+        local char = Fetch:Source(source):GetData("Character")
+        Inventory.Items:Remove(char:GetData("SID"), 1, "money_checkque", 1)
+
+        -- Banking.Balance:Deposit(
+        --     Banking.Accounts:GetPersonal(char:GetData("SID")).Account,
+        --     count * v.price,
+        --     {
+        --         type = "deposit",
+        --         title = "Goods Export",
+        --         description = string.format(
+        --             "Sold %s x%s at $%s/unit",
+        --             itemData.label,
+        --             count,
+        --             v.price
+        --         ),
+        --     }
+        -- )
+    end)
+
+    Callbacks:RegisterServerCallback('Labor:Server:MoneyLaunder:AlertPolice', function(source, data, cb)
+        Robbery:TriggerPDAlert(source, coords, "10-31A", "Illegal Cash Deposit", {
+            icon = 568,
+            size = 0.9,
+            color = 1,
+            duration = (60 * 5),
+        })
+    end)
+
+    Callbacks:RegisterServerCallback('Labor:Server:MoneyLaunder:Finish', function(source, data, cb)
+        Phone.Notification:Add(
+            source,
+            "Labor Activity",
+            "Run Complete!",
+            os.time() * 1000,
+            6000,
+            "labor",
+            {}
+        )
+    end)
+
+    Callbacks:RegisterServerCallback('Labor:Server:MoneyLaunder:Trade', function(source, data, cb)
+        if data.item == 'moneyroll' or data.item == 'moneyband'  then
+            local char = Fetch:Source(source):GetData("Character")
+
+            if data.item == 'moneyroll' then
+                Inventory.Items:Remove(char:GetData("SID"), 1, "moneyroll", 50)
+                Inventory:AddItem(char:GetData("SID"), "money_checkque", 1, {}, 1)
+            end
+
+            if data.item == 'moneyband' then
+                Inventory.Items:Remove(char:GetData("SID"), 1, "moneyband", 5)
+                Inventory:AddItem(char:GetData("SID"), "money_checkque", 1, {}, 1)
+            end
+        end
+    end)
+end)
