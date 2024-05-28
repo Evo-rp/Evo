717ad23299e6f6fa7912fc39a8468ef0a9c6904d
Blooper <118882930+Blooper4921@users.noreply.github.com>
2024-05-28 14:10:41 +0100

    gabz catcafe update

--- resources/[main]/[assets]/[gabz]/cfx-gabz-prison/fxmanifest.lua
+++ resources/[main]/[assets]/[gabz]/cfx-gabz-prison/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 author 'Gabz'
 description 'Prison'
-version '1.0.0'
+version '1.0.2'
 lua54 'yes'
 this_is_a_map 'yes'
 
@@ -12,11 +12,16 @@ dependencies {
     'cfx-gabz-mapdata', -- ⚠️PLEASE READ⚠️; Requires [cfx-gabz-mapdata] to work properly.
 }
 
+files {
+    'cfx-gabz-prison_cache_y.dat'
+}
+
 server_scripts {
     'version_check.lua',
 }
 
 escrow_ignore {
     'stream/**/*.ytd',
+    'stream/vanilla/*.*',
 }
 dependency '/assetpacks'
