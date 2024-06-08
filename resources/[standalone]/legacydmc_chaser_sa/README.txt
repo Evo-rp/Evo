fa28c45a408a7d9fc904c0e0d7fc156430cdaa78
Blooper <118882930+Blooper4921@users.noreply.github.com>
2024-06-08 01:53:10 +0100

    init: chaser

---
+++ resources/[standalone]/legacydmc_chaser_sa/README.txt
@@ -0,0 +1,10 @@
+Hi, thank you for acquiring the C.H.A.S.E.R driving system.
+
+You may find the Owners Manual containing informations on how to use on a followup email sent
+alongside the purchase email from tebex on your emails provider.
+
+Remember to redo the first startup procedures when updating.
+
+Alternatively, you can download it and check for changelogs and more at:
+
+https://legacydmc.net/chaser/
---
+++ resources/[standalone]/legacydmc_chaser_sa/config/chaserconfig.lua
@@ -0,0 +1,18 @@
+Chaserconfig = {} --- Do Not Change, edit, or add new values, may cause issues.
+---------------------------------------------------------------------------------GENERAL SETTINGS------------------------------------------------------------------------------------
+Chaserconfig.enablecustombasevents = true
+Chaserconfig.enableintegratedhud = true
+Chaserconfig.integratedhudrefreshrate = 11
+Chaserconfig.showhudspeedinmph = false
+Chaserconfig.simulationdriftmode = false
+Chaserconfig.enableautosequencial = true
+Chaserconfig.enabledownshiftprotection = true
+Chaserconfig.pp_usealternativetopspeedratecalc = true
+Chaserconfig.pp_maxavgacc = 1.17
+Chaserconfig.pp_minavgacc = 0.06
+Chaserconfig.pp_maxspeedrate = 280
+Chaserconfig.pp_minpeakgriprate = 1.6
+Chaserconfig.pp_maxpeakgriprate = 2.6
+Chaserconfig.pp_minpeakbikegriprate = 1.0
+Chaserconfig.pp_maxpeakbikegriprate = 1.6
+---------------------------------------------------------------------------------GENERAL SETTINGS------------------------------------------------------------------------------------
