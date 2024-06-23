b2846f3736101a231c5d9f6e1000c21af8d8dfcd
Blooper <118882930+Blooper4921@users.noreply.github.com>
2024-06-23 02:10:36 +0100

    Update deliveries.lua

--- resources/[evo]/evo-labor/client/jobs/deliveries.lua
+++ resources/[evo]/evo-labor/client/jobs/deliveries.lua
@@ -198,6 +198,7 @@ RegisterNetEvent("Deliveries:Client:OnDuty", function(joiner, time)
 		Callbacks:ServerCallback("Deliveries:DeliverPackage", {}, function(s)
 			if s then
 				LocalPlayer.state.hasDeliveryPackage = false
+				Wait(1000)
 				ClearPedTasksImmediately(PlayerPedId())
 			end
 		end)
